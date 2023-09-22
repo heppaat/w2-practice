0d3d651b0faa85f2c2d1027c2049da4b09a90a55
heppaat <pali.herrer@gmail.com>
2023-09-22 20:07:30 +0200

    css layout on data

--- script.js
+++ script.js
@@ -14,9 +14,34 @@ while (counter < 3) {
   counter++;
 } */
 
-l; /* et array = ["alma", "korte", "eper", "birs"];
+/* let array = ["alma", "korte", "eper", "birs"];
 
 for (let i = 0; i < array.length; i++) {
   rootElement.insertAdjacentHTML("beforeend", `<h2> ${array[i]} </h2>`);
 }
  */
+
+fetch("https://restcountries.com/v3.1/all")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data); //itt tudunk a kapott adattal dolgozni
+
+    for (let index = 0; index < data.length; index++) {
+      let countryName = data[index].name.common;
+      let countryPop = data[index].population;
+      let countryArea = data[index].area;
+
+      rootElement.insertAdjacentHTML(
+        "beforeend",
+        `
+      <div class="country">
+        <h2> ${countryName} </h2>
+        <p> population: ${countryPop} </p>
+        <p> area: ${countryArea} km2 </p>
+      </div>
+      `
+      );
+    }
+  });
